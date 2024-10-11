e518e803bc753819319b1f69ca771aeefa2c9d72
masuim <po.n31227@gmail.com>
2024-10-11 17:34:59 +0900

    fix: Headerの見た目修正、アイコン追加

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 type HeaderProps = {
 setCurrentView: (view: 'dashboard' | 'niccaList') => void;
@@ -6,23 +7,31 @@ setCurrentView: (view: 'dashboard' | 'niccaList') => void;
 
 export const Header = ({ setCurrentView }: HeaderProps) => {
 return (
-<header className="absolute top-0 left-0 right-0 z-10">
-    <nav className="container mx-auto flex justify-between items-center py-4">
-        <div className="flex space-x-4">アプリのアイコンが入る</div>
+<header className="absolute top-0 left-0 right-0 z-10 bg-mainColor text-white">
+    <nav className="container mx-auto flex justify-between items-center py-4 h-16">
+        <div className="flex items-center h-full">
+            <Image
+                src="/images/logos/bg-removed-logo.png"
+                alt="app logo"
+                width={60}
+                height={60}
+                className="object-contain"
+            />
+        </div>
         <div className="flex space-x-4">
-            <Button variant="ghost" onClick={()=> setCurrentView('dashboard')}>
+            <Button variant="ghost" className="text-white " onClick={()=> setCurrentView('dashboard')}>
                 ダッシュボード
             </Button>
-            <Button variant="ghost" onClick={()=> setCurrentView('niccaList')}>
+            <Button variant="ghost" className="text-white " onClick={()=> setCurrentView('niccaList')}>
                 日課一覧
             </Button>
-            <Button variant="ghost" onClick={()=> alert('日課登録 Clicked!!')}>
+            <Button variant="ghost" className="text-white " onClick={()=> alert('日課登録 Clicked!!')}>
                 日課登録
             </Button>
-            <Button variant="ghost" onClick={()=> alert('ユーザー設定 Clicked!!')}>
+            <Button variant="ghost" className="text-white " onClick={()=> alert('ユーザー設定 Clicked!!')}>
                 ユーザー設定
             </Button>
-            <Button variant="ghost" onClick={()=> alert('サインアウト Clicked!!')}>
+            <Button variant="ghost" className="text-white " onClick={()=> alert('サインアウト Clicked!!')}>
                 サインアウト
             </Button>
         </div>
