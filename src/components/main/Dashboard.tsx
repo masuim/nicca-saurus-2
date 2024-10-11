c899b919ef4450356c22730472df5a523d0c576f
masuim <po.n31227@gmail.com>
2024-10-12 08:21:28 +0900

    add: saurusImageコンポーネント

--- src/components/main/Dashboard.tsx
+++ src/components/main/Dashboard.tsx
@@ -1,10 +1,18 @@
 import { CustomCalendar } from "@/components/main/Dashboard/Calendar";
+import { SaurusImage } from "@/components/main/Dashboard/SaurusImage";
 
 type Props = {
   currentView: 'dashboard';
 }
 
+
 export const Dashboard = ({ currentView }: Props) => {
-  return (<div><CustomCalendar /></div>);
+
+  // TODO: Move to schemas
+  const SAURUS_TYPES = ['brachiosaurus', 'triceratops', 'pteranodon', 'tyrannosaurus'];
+  return (<div>
+    <CustomCalendar />
+    <SaurusImage saurusType={SAURUS_TYPES[0]} />
+    </div>);
 };
 
---
+++ src/components/main/Dashboard/SaurusImage.tsx
@@ -0,0 +1,23 @@
+
+
+type Props = {
+    // TODO: schema作成したらstringではなくなる
+  saurusType: string;
+  className?: string;
+};
+
+export const SaurusImage = ({ saurusType, className }: Props) => {
+  // TODO: "1.png"で固定値になっているが動的にする
+  const imagePath = `/images/saurus/${saurusType}/${saurusType}1.png`;
+  return (
+    <div className={`mb-4 ${className}`}>
+      <img
+        src={imagePath}
+        alt={saurusType}
+        width={200}
+        height={200}
+        className="rounded-lg shadow-lg"
+      />
+    </div>
+  );
+};
