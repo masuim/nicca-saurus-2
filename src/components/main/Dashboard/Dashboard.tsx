0408c4d8130a6f7161a38bcb7cff22c6f8f6eff2
masuim <po.n31227@gmail.com>
2024-10-12 17:45:08 +0900

    fix: dashboardレイアウト

--- src/components/main/Dashboard/Dashboard.tsx
+++ src/components/main/Dashboard/Dashboard.tsx
@@ -14,13 +14,13 @@ export const Dashboard = ({ currentView }: Props) => {
   return (
     <div className="sm:main-background xs mx-auto mt-4 w-[calc(100%-1rem)] max-w-[280px] rounded-lg p-4 xs:w-[calc(100%-2rem)] xs:max-w-[360px] sm:w-full sm:max-w-[640px] sm:border-2 sm:border-mainColor sm:p-6 md:p-8 lg:mt-8 lg:max-w-[720px]">
       <div className="mb-8">
-        <NiccaMessage className="dashboard-component relative flex h-full items-center border-2 border-mainColor bg-[#ECEFF4] p-6" />
+        <NiccaMessage className="dashboard-component relative flex h-full items-center border-2 border-mainColor bg-gray-50 p-6" />
       </div>
       <div className="grid grid-cols-1 gap-8 sm:grid-cols-2">
         <div className="h-full w-full">
-          <CustomCalendar className="dashboard-component h-full w-full bg-[#ECEFF4]" />
+          <CustomCalendar className="dashboard-component h-full w-full bg-gray-50" />
         </div>
-        <div className="dashboard-component flex h-full flex-col justify-between border-2 border-mainColor bg-[#ECEFF4] p-6">
+        <div className="dashboard-component flex h-full flex-col justify-between border-2 border-mainColor bg-gray-50 p-6">
           <CompleteButton className="mb-4 w-full transform rounded-lg border-2 border-mainColor py-3 text-lg font-bold text-white transition-all duration-300 hover:scale-105 hover:shadow-xl" />
           <div className="flex flex-grow transform items-center justify-center overflow-hidden transition-all duration-300 hover:scale-105 hover:shadow-lg">
             <SaurusImage saurusType={SAURUS_TYPES[0]} className="w-full" />
@@ -29,7 +29,7 @@ export const Dashboard = ({ currentView }: Props) => {
         </div>
       </div>
       <div className="mt-8">
-        <AchievementMetrics className="dashboard-component bg-[#ECEFF4]" />
+        <AchievementMetrics className="dashboard-component bg-gray-50" />
       </div>
     </div>
   );
