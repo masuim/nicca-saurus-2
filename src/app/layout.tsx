483fc906f8281e1f281dc68965f7f0ccb5ed842b
masuim <po.n31227@gmail.com>
2024-10-15 20:16:21 +0900

    fix: sessionProvider追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,23 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
 import Image from 'next/image';
+import { NextAuthSessionProvider } from '@/providers/SessionProvider';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
 });
 
 export const metadata: Metadata = {
-  title: "Nicca Saurus",
-  description: "Nicca Saurus",
+  title: 'Nicca Saurus',
+  description: 'Nicca Saurus',
 };
 
 export default function RootLayout({
@@ -26,19 +27,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased relative min-h-screen`}
-      >
-        <Image
-          src="/images/background/bg-white-overlay-10.png"
-          fill={true}
-          style={{ objectFit: 'cover' }}
-          quality={100}
-          alt="Background"
-          className="z-0"
-        />
-        <div className="relative z-10">{children}</div>
-      </body>
+      <NextAuthSessionProvider>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} relative min-h-screen antialiased`}
+        >
+          <Image
+            src="/images/background/bg-white-overlay-10.png"
+            fill={true}
+            style={{ objectFit: 'cover' }}
+            quality={100}
+            alt="Background"
+            className="z-0"
+          />
+          <div className="relative z-10">{children}</div>
+        </body>
+      </NextAuthSessionProvider>
     </html>
   );
 }
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,5 +1,28 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 import { MainContent } from '@/components/main/MainContent';
 
 export default function MainPage() {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === 'loading') return;
+    if (!session) {
+      router.push('/');
+    }
+  }, [session, status, router]);
+
+  if (status === 'loading') {
+    return <div>ローディング中...</div>;
+  }
+
+  if (!session) {
+    return null;
+  }
+
   return <MainContent />;
 }
