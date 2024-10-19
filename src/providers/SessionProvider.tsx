8d33aa761bd795ec6a7a942783fa898bdcc19e72
masuim <po.n31227@gmail.com>
2024-10-19 12:22:30 +0900

    fix: アロー関数に書き換え

--- src/providers/SessionProvider.tsx
+++ src/providers/SessionProvider.tsx
@@ -2,6 +2,6 @@
 
 import { SessionProvider } from 'next-auth/react';
 
-export function NextAuthSessionProvider({ children }: { children: React.ReactNode }) {
+export const NextAuthSessionProvider = ({ children }: { children: React.ReactNode }) => {
   return <SessionProvider>{children}</SessionProvider>;
-}
+};
