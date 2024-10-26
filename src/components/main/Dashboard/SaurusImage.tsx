5d6c98f0d7f2dfd9e29ef3c6afc4f9c5e732e48f
masuim <po.n31227@gmail.com>
2024-10-26 19:50:47 +0900

    fix: 画像のだし分けロジック実装

--- src/components/main/Dashboard/SaurusImage.tsx
+++ src/components/main/Dashboard/SaurusImage.tsx
@@ -1,16 +1,27 @@
+import { useMemo } from 'react';
+import { Nicca } from '@/types/nicca';
+
 type Props = {
-  // TODO: schema作成したらstringではなくなる
-  saurusType: string;
+  nicca: Nicca;
   className?: string;
 };
 
-export const SaurusImage = ({ saurusType, className }: Props) => {
-  const imagePath = `/images/saurus/${saurusType}/${saurusType}1.png`;
+export const SaurusImage = ({ nicca, className }: Props) => {
+  const saurusLevel = useMemo(() => {
+    const weekDays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+    const selectedDaysCount = weekDays.filter((day) => nicca[day as keyof Nicca]).length;
+    const achievementsCount = nicca.achievements.length;
+    const level = Math.floor(achievementsCount / selectedDaysCount) + 1;
+    return Math.min(level, 5);
+  }, [nicca]);
+
+  const imagePath = `/images/saurus/${nicca.saurusType}/${nicca.saurusType}${saurusLevel}.png`;
+
   return (
     <div className={`${className} relative overflow-hidden`} style={{ paddingTop: '100%' }}>
       <img
         src={imagePath}
-        alt={saurusType}
+        alt={nicca.saurusType}
         className="absolute inset-0 h-full w-full rounded-lg object-cover hover:shadow-lg"
       />
     </div>
